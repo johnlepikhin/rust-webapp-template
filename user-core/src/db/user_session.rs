fd02f66240807f46d54bf47f1a130f6c4f3447c9
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2023-11-20 21:58:17 +0300

    feat: Generate user sessions

--- user-core/src/db/user_session.rs
+++ user-core/src/db/user_session.rs
@@ -1,5 +1,7 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use diesel::prelude::*;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use test_db::schema::user_session;
 
 #[derive(Identifiable, Queryable, PartialEq, Eq, Debug, Clone)]
@@ -7,7 +9,7 @@ use test_db::schema::user_session;
 pub struct UserSession {
     pub id: i64,
     pub user_id: Option<i64>,
-    token: String,
+    pub token: String,
     pub create_date: chrono::DateTime<chrono::Utc>,
     pub last_seen_date: chrono::DateTime<chrono::Utc>,
     pub requests_count: i64,
@@ -26,9 +28,14 @@ pub struct UserSessionNew {
 }
 
 impl UserSessionNew {
-    pub fn new(token: String, last_address: ipnet::IpNet) -> Self {
+    pub fn new(user: &crate::db::user::User, last_address: ipnet::IpNet) -> Self {
+        let token: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(64)
+            .map(char::from)
+            .collect();
         Self {
-            user_id: None,
+            user_id: Some(user.id),
             token,
             create_date: chrono::Utc::now(),
             last_seen_date: chrono::Utc::now(),
@@ -46,10 +53,15 @@ impl UserSessionNew {
 }
 
 impl UserSession {
-    pub fn new(db: &mut diesel::PgConnection, session: UserSessionNew) -> Result<UserSession> {
+    pub fn new(
+        db: &mut diesel::PgConnection,
+        user: &crate::db::user::User,
+        last_address: ipnet::IpNet,
+    ) -> Result<UserSession> {
         let r = diesel::insert_into(user_session::dsl::user_session)
-            .values(session)
-            .get_result(db)?;
+            .values(UserSessionNew::new(user, last_address))
+            .get_result(db)
+            .map_err(|err| anyhow!("Failed to add user session: {err}"))?;
         Ok(r)
     }
 }
