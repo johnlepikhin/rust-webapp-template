2799b066d1ed63342e0b262feb9cb8742c0282bb
Evgenii Lepikhin <e.lepihin@postgrespro.ru>
2023-11-27 19:25:19 +0300

    refactor: Cleanup

--- user-core/doc/migrations/2023-10-19-180742_user-core/down.sql
+++
@@ -1,5 +0,0 @@
-
-drop index user_username;
-drop index user_session_token;
-drop table user_session;
-drop table "user";
--- user-password-auth/src/config.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use structdoc::StructDoc;
-
-#[derive(Serialize, Deserialize, StructDoc)]
-pub struct Config {
-    /// Minimum password length
-    pub min_password_length: usize,
-}
